0bdd5c618f55f314c1d377c2532596338d42fb1d
powellniki <153695840+powellniki@users.noreply.github.com>
2024-01-04 17:08:52 -0600

    updated files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let addBookButton = document.getElementById("addBookButtonEl")
+let bookCount = document.getElementById("bookCountEl")
+let totalBookCount = 0
+
+function increment() {
+    totalBookCount += 1
+    bookCount.textContent = totalBookCount
+}
